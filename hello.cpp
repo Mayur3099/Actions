4629faf836a155dd50c407e301d4bab211a6e24a
Mayur3099 <139243145+Mayur3099@users.noreply.github.com>
2024-01-25 17:47:30 +0530

    Initial Commit

---
+++ hello.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+using ll = long long;
+ll LongLongMax = numeric_limits<ll>::max();
+
+const int MOD = 1e9+7;
+
+void solve()
+{
+    cout<<"avjnfjdavjfdbvjdrfr\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    solve();
+
+    return 0;
+}
